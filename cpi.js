c32df2911d1402022d82b407ff67420ac9891a3c
vkwd <vkwd@users.noreply.github.com>
2021-05-05 20:29:25 +0530

    code changed on revised figures

--- cpi.js
+++ cpi.js
@@ -3,14 +3,12 @@ var d = document.querySelector("#btnm"),
     h = document.querySelector("div.mve"),
     k = document.querySelector("#psbe"),
     m, n;
+const conv_factor = 2.88 * 4.63 * 4.93;
 
 function p() {
     if (isNaN(q()) || 3500 > q() || 250000 < q()) {
         return h.style.display = "none", r.textContent = " Basic Pay invalid-Reset", !0;
     }
-    // if (isNaN(parseFloat(document.querySelector("#index3").value))) {
-    //     return r.textContent = "index no is  invalid", !0;
-    // }
     r.textContent = "";
     return !1;
 }
@@ -26,20 +24,27 @@ document.querySelector("#btn").addEventListener("click", function() {
             b = parseFloat(document.querySelector("#index2").value),
             c = parseFloat(document.querySelector("#index3").value),
             l = q();
-        a = +(65.73892 * a).toFixed(2);
-        b = +(65.73892 * b).toFixed(2);
-        c = +(65.73892 * c).toFixed(2);
+        a = +(conv_factor * a).toFixed(2);
+        b = +(conv_factor * b).toFixed(2);
+        c = +(conv_factor * c).toFixed(2);
+        console.log(a, b, c);
         b = +((a + b + c) / 3).toFixed(2);
+        //console.log(b);
         if (1 == k.checked) {
-            return m = 0.2618, n = 374, e = Math.floor((b - 6352) / 4), f = 7E-4 * e, f = +f.toFixed(4), a = l * f - .2618 * l, a = +a.toFixed(2), t(b, a), !1;
+            return m = 0.2618, n = 374, e = Math.round((b - 6352) / 4),
+                f = 7E-4 * e, f = +f.toFixed(4), a = l * f - .2618 * l, a = +a.toFixed(2), t(b, a), console.log(a, e, f, );
+            !1;
+
         }
         n = 852;
         m = 0.852;
-        e = Math.floor((b - 4440) / 4);
+        e = Math.round((b - 4440) / 4);
+        console.log(e);
         f = .001 * e;
         f = +f.toFixed(4);
         a = l * f - l * m;
         a = +a.toFixed(2);
+        //console.log(e, f, a);
         t(b, a);
 
     }
@@ -53,8 +58,9 @@ d.addEventListener("click", function() {
 
 function t(a, b) {
     var c = e;
+    console.log(c);
     document.querySelector("#first").innerHTML = " Last Quarterly Average(QA) <b>:7849<br>\n  DA rate-quarter ending April 21 : " + (100 * m).toFixed(2) + "%\n   <br> Slabs paid : " + n;
-    document.querySelector("#second").innerHTML = "<br>\n QA (Jan,Feb,March 21) <b>: " + Math.round(a) + "\n   <br> Slabs payable : " + c + "\n <br> Gain/Loss in slabs : " + (c - n);
+    document.querySelector("#second").innerHTML = "<br>\n QA (Jan,Feb,March 21) <b>: " + (a) + "\n   <br> Slabs payable : " + c + "\n <br> Gain/Loss in slabs : " + (c - n);
     document.querySelector("#third").innerHTML = "\n <br>DA rate for quarter (May, June, July 21): \n " + f + "\n<br>DA payable for the quarter : \n<br>" + (100 * f).toFixed(2) + "% of basic pay";
     document.querySelector("#fourth").innerHTML = "\nGain/loss over last quarter: \u20b9 " + b + "\n" + "<br>";
     document.querySelector("#fifth").innerHTML = "<br>\n There shall be no change in Payment of DR to Pensioners. DR, in their case, will be reviewed on July 30, 21 after the CPI data for  June 21 is released.";
